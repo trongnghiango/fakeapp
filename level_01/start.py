ffb02b858cf326114e9b95390e83b5a1f4ddc443
NGHIA <trongnghia.ngo@hotmail.com>
2023-12-22 12:25:35 +0700

    level-01: demo
    ok

--- level_01/start.py
+++ level_01/start.py
@@ -1,4 +1,9 @@
+
+"""
+Bien va Kieu du lieu
+"""
 x = 10 # Khai báo biến x và khởi tạo với value: 10
-type(x) #Check kiểu của biến x là gì?
+xType = type(x) #Check kiểu của biến x là gì?
 xAdr = id(x) # id là lấy địa chỉ vùng nhớ của x dạng cơ số 10 
 hexAdr = hex(xAdr) # địa chỉ vùng nhớ dạng hex của biến x
+print(f"Bien x co gia tri {x} co kieu du lieu {xType} luu ow Addr: {hexAdr}")
